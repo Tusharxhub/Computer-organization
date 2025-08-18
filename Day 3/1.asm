872def7cb8eff42874cb08f7e5028cba59e22271
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-18 10:37:44 +0530

    Add comments to 8085 program for adding two 16-bit numbers

---
+++ Day 3/1.asm
@@ -0,0 +1,4 @@
+;wap 8085 program to add two 16 bit number  
+;the first number is stored at memory location 2100H(low byte) and 2101H(high byte).
+;the second number is stored at memory location 2102H(low byte) and 2103H(high byte).
+;store the 16 bit result at memory location 2104H(low byte) and 2105H(high byte), and any carry at 2106H.
