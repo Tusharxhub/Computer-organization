a5e8879e996967ca43807a6f17d0f68d27e34a1a
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-25 10:48:44 +0530

    Refactor and enhance comments in 8085 program for adding two 8-bit numbers

--- Day 5/1.asm
+++ Day 5/1.asm
@@ -1,2 +1,21 @@
-;wap a 8085 program to add two 8-bit numbers stored in memory locations 2070H and 2071H
-;useing the method of repetitive addition
+; Program: Add two 8-bit numbers stored in 2070H and 2071H 
+; using repetitive addition
+; Result is stored at 2072H
+
+    LDA 2070H       ; Load first number into A
+    MOV C,A         ; Copy first number to C (multiplicand)
+    LDA 2071H       ; Load second number into A
+    MOV B,A         ; Copy second number to B (multiplier / counter)
+
+    MVI A,00H       ; Clear accumulator (to store sum)
+    MOV D,A         ; D = 00H (for storing sum)
+    
+ADD_LOOP: 
+    ADD C           ; Add first number (C) to accumulator
+    DCR B           ; Decrement counter
+    JNZ ADD_LOOP    ; Repeat until B = 0
+
+    STA 2072H       ; Store result at 2072H
+    HLT             ; Stop
+
+
