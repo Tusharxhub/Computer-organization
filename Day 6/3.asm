49cea6160e1016545c2f9f167753aea6f37c5fd0
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-11-10 10:50:25 +0530

    Refactor assembly program to include complete logic for arranging an array in both ascending and descending order

--- Day 6/3.asm
+++ Day 6/3.asm
@@ -1,3 +1,46 @@
 ;write to 8085 assembly language programme to arrange an array of data in ascending and descending order.
 
 
+;Ascending: 
+
+ORG 2000H
+LXI H, 2050H
+MVI B, 05H ; Number of elements
+OUTER: LXI H, 2050H
+MVI C, 04H ; Inner loop count = size - 1
+INNER: MOV A, M
+INX H
+CMP M
+JC SKIP ; If A < M, skip
+MOV D, M ; Swap values
+MOV M, A
+DCX H
+MOV M, D
+INX H
+SKIP: DCR C
+JNZ INNER
+DCR B
+JNZ OUTER
+HLT
+
+;Descending:
+
+ORG 2100H
+LXI H, 2050H
+MVI B, 05H ; Number of elements
+OUTER: LXI H, 2050H
+MVI C, 04H ; Inner loop count = size - 1
+INNER: MOV A, M
+INX H
+CMP M
+JNC SKIP ; If A >= M, skip
+MOV D, M ; Swap values
+MOV M, A
+DCX H
+MOV M, D
+INX H
+SKIP: DCR C
+JNZ INNER
+DCR B
+JNZ OUTER
+HLT
