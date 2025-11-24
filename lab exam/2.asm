12fb6f080fd89929183aae101577e1c160ef4633
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-11-24 10:35:55 +0530

    Add VSDL module design for 1-bit input addition using Half Adder and 2-to-1 multiplexer

---
+++ lab exam/2.asm
@@ -0,0 +1,3 @@
+;Design a VSDL module that : Adds 1-bit input using Half Adder,Then uses a 2 to 1 multiplexer to select between the SUM and CARRY output . Inputs:A,B,SEL Outputs:Y
+
+;Test the circuit using multiple input combinations in EDA playground
