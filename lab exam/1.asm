d02eda0a1850117b39dbb4880144fac5497d5479
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-11-24 10:31:02 +0530

    Implement 8085 assembly program for sorting an array using bubble sort algorithm

--- lab exam/1.asm
+++ lab exam/1.asm
@@ -2,39 +2,38 @@
 
 
 
-; Program: Sort an array in ascending order (Bubble Sort)
-; Processor: Intel 8085
-; Assumptions:
-;   - Array starts at memory location 9000H
-;   - First byte at 9000H = number of elements (N)
-;   - Next N bytes = transaction amounts
-;   - Sorted result will overwrite the same memory locations
+; A smallembedded controller program powered by the intel 8085 processor is used inside a banking terminal for handling secure data movements.arithmetic operations , and basic logical decision-making.During testing,the following operational requirements were identified.You must design efficent 8085 assembly language programs foe each task described below - The banking terminal stores multiple transaction amounts in an array. write programs to arrange the array in assending order.
 
-        ORG 8000H        ; Program start address
 
-START:  LXI H, 9000H     ; HL points to array start
-        MOV B, M         ; B = N (number of elements)
-        DCR B            ; B = N-1 (passes required)
+; Assume array length at 8000H, elements from 8001H onwards
 
-PASS:   LXI H, 9001H     ; HL points to first element
-        MOV C, B         ; C = number of comparisons in this pass
+LXI H,8000H       ; HL = starting address of array
+MOV C, M          ; C = number of elements, from 8000H
+DCR C             ; For (n-1) passes
 
-COMPARE:MOV A, M         ; A = current element
-        INX H            ; HL -> next element
-        CMP M            ; Compare A with next element
-        JC NOSWAP        ; If A < M, no swap needed
+LOOP1:            ; Outer loop for passes
+MOV B, C          ; B = (n-1), inner swaps
+LXI H,8001H       ; HL = 1st data element address
 
-        ; Swap elements
-        MOV D, M         ; D = next element
-        MOV M, A         ; Store smaller value in next location
-        DCX H            ; HL -> previous element
-        MOV M, D         ; Store larger value in previous location
-        INX H            ; HL -> next element again
+LOOP2:            ; Inner loop for swapping
+MOV A, M          ; A = array[i]
+INX H
+MOV D, M          ; D = array[i+1]
 
-NOSWAP: DCR C            ; Decrement comparison count
-        JNZ COMPARE      ; Repeat until C = 0
+CMP D             ; Compare array[i] with array[i+1]
+JC NO_SWAP        ; If array[i]<array[i+1], do nothing
 
-        DCR B            ; Decrement pass count
-        JNZ PASS         ; Repeat passes until sorted
+; Swap array[i] and array[i+1]
+MOV M, A          ; array[i+1] = array[i]
+DCX H
+MOV M, D          ; array[i] = array[i+1]
+INX H
 
-        HLT              ; End of program
+NO_SWAP:
+DCR B
+JNZ LOOP2         ; Repeat inner loop
+
+DCR C
+JNZ LOOP1         ; Repeat outer loop
+
+HLT               ; Stop program
